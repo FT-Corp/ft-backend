2888925f40db30b63117883fd20c8a6c303fafb4
mochangil <ahckddlf1234@gmail.com>
2024-09-23 17:30:19 +0900

    Feat: Add BaseEntity with TimeAuditor

---
+++ src/main/java/com/spring/ftbackend/common/config/JpaAuditorConfig.java
@@ -0,0 +1,12 @@
+package com.spring.ftbackend.common.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing(
+        dateTimeProviderRef = "timeAuditorAware"
+)
+@Configuration
+public class JpaAuditorConfig {
+}
