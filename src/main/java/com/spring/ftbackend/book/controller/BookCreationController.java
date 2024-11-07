46f56109a4bbde098fdbc04e45eba73d6d9bee68
sall <keinpark1@naver.com>
2024-11-07 19:09:29 +0900

    docs: `streamProgress` 메소드에 책 생성 진행 상황 SSE 스트리밍 Swagger 설명 추가

--- src/main/java/com/spring/ftbackend/book/controller/BookCreationController.java
+++ src/main/java/com/spring/ftbackend/book/controller/BookCreationController.java
@@ -1,6 +1,7 @@
 package com.spring.ftbackend.book.controller;
 
 import com.spring.ftbackend.book.service.SseEmitterService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ public class BookCreationController {
     @Autowired
     private SseEmitterService sseEmitterService;
 
+    @Operation(summary = "책 생성 진행 상황 SSE 스트리밍")
     @GetMapping(value = "/progress")
     public SseEmitter streamProgress() {
         return sseEmitterService.createEmitter();
