b8a6682e49af0b4d205f3ff5ccd2ff855a125468
sall <keinpark1@naver.com>
2024-11-07 19:08:23 +0900

    feat: 책 100프로 생성시 sse연결 끊어주는 처리 추가

--- src/main/java/com/spring/ftbackend/book/service/SseEmitterService.java
+++ src/main/java/com/spring/ftbackend/book/service/SseEmitterService.java
@@ -35,6 +35,11 @@ public class SseEmitterService {
                     emitter.send(SseEmitter.event()
                             .name("progress")
                             .data("Book:"+bookName+"  Progress:"+progress+"%"));
+                    // progress가 100이면 전송 완료 후 연결 종료
+                    if (progress == 100) {
+                        emitter.complete(); // 연결 종료
+                        emitters.remove(emitter); // 종료 후 emitters에서 제거
+                    }
                 } catch (IOException e) {
                     emitters.remove(emitter); // 전송 실패 시 emitter 제거
                 }
