39453da36ed49283cbc836d475b971395bf89cfa
mochangil <ahckddlf1234@gmail.com>
2024-11-10 15:21:36 +0900

    Chore: 쿠폰 및 크레딧 swagger 설명 등록

--- src/main/java/com/spring/ftbackend/credit/controller/CreditController.java
+++ src/main/java/com/spring/ftbackend/credit/controller/CreditController.java
@@ -5,6 +5,7 @@ import com.spring.ftbackend.credit.dto.request.UserCreditSaveRequest;
 import com.spring.ftbackend.credit.dto.response.CreditResponse;
 import com.spring.ftbackend.credit.service.CreditService;
 import com.spring.ftbackend.credit.service.UserCreditService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,24 +24,28 @@ public class CreditController {
     private final CreditService creditService;
     private final UserCreditService userCreditService;
 
+    @Operation(summary = "크레딧 등록")
     @PostMapping
     public ResponseEntity<CreditResponse> saveCredit(@RequestBody CreditSaveRequest request) {
         CreditResponse creditResponse = creditService.saveCredit(request);
         return ResponseEntity.ok(creditResponse);
     }
 
+    @Operation(summary = "유저가 보유한 총 크레딧 액수 조회")
     @GetMapping("/user")
     public ResponseEntity<Integer> getUserCredit(@RequestParam Long userId) {
         Integer totalCreditAmount = userCreditService.getUserCreditAmount(userId);
         return ResponseEntity.ok(totalCreditAmount);
     }
 
+    @Operation(summary = "유저에게 크레딧 등록")
     @PostMapping("/user")
     public ResponseEntity<CreditResponse> registerUserCredit(@RequestBody UserCreditSaveRequest request) {
         userCreditService.registerUserCredit(request);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "유저가 보유한 크레딧 건수 조회")
     @GetMapping("/user/list")
     public ResponseEntity<List<CreditResponse>> getUserCreditList(@RequestParam Long userId) {
         List<CreditResponse> creditResponseList = userCreditService.getUserCreditList(userId);
